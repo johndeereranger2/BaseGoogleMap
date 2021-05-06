13ee7f4d87fc965d5218234b654e1786af848e91
johndeereranger2 <78242025+johndeereranger2@users.noreply.github.com>
2021-05-06 12:45:01 -0400

    Update MarkerLocationRealmManager.kt

--- app/src/main/java/com/deerbrain/googlemapsbase/HeatMaps/MarkerLocationRealmManager.kt
+++ app/src/main/java/com/deerbrain/googlemapsbase/HeatMaps/MarkerLocationRealmManager.kt
@@ -6,6 +6,8 @@ import com.deerbrain.googlemapsbase.Realm.RealmWrapper
 import com.google.android.gms.maps.model.LatLng
 
 class MarkerLocationRealmManager {
+    
+    //https://medium.com/@markosullivan/realm-multi-level-backlinks-inverse-relationships-with-linking-objects-836559ebec8
 
     var id = (RealmWrapper.realm.where(MarkerLocation::class.java).max("id")?.toInt() ?: 0) + 1
 
@@ -23,9 +25,14 @@ class MarkerLocationRealmManager {
 
     fun northWestMarker(x: Int, y: Int, mapName: MapNames) : MarkerLocation? {
 
-        val map = RealmWrapper.realm.objects<MapNames>()
+     //   val map = RealmWrapper.realm.objects<MapNames>()
         //return mapName.theMarkerLocation.filter("row == %@ x and col == %@" , y + 1, x).first
-        return RealmWrapper.realm.where(MapNames::class.java).equalTo("row",x).findFirst()
+    //return RealmWrapper.realm.where(MapNames::class.java).equalTo("row",x).findFirst()
+        
+        return RealmWrapper.realm.where(MapNames::class.java)
+            .equalTo("theMarkerLocation.row",x)
+            .equalTo("theMarkerLocation.col", y +1 )
+            .findFirst()
     }
 
     fun maxDistance(mapName: MapNames) : Double? {
