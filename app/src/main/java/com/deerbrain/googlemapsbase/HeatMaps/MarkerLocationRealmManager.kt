d97f004c0cfde06e46c903e7923c588d5fc70e44
johndeereranger2 <78242025+johndeereranger2@users.noreply.github.com>
2021-05-07 11:05:39 -0400

    Update MarkerLocationRealmManager.kt
    another realm update

--- app/src/main/java/com/deerbrain/googlemapsbase/HeatMaps/MarkerLocationRealmManager.kt
+++ app/src/main/java/com/deerbrain/googlemapsbase/HeatMaps/MarkerLocationRealmManager.kt
@@ -39,6 +39,29 @@ class MarkerLocationRealmManager {
     //https://stackoverflow.com/questions/49483520/how-to-use-linkingobject-for-android
        fun northWestMarker(x: Int, y: Int, mapName: MapNames) : Double {
 
+           //check on this result
+           return RealmWrapper.realm.where(MapNames::class.java)
+            .equalTo("theMarkerLocation.row",x)
+            .equalTo("theMarkerLocation.col", y +1 )
+            .findFirst().getDouble("distanceNearMarker")
+//            https://docs.mongodb.com/realm-legacy/docs/kotlin/latest/index.html
+//            // All fields are accessed using strings
+// val name = person.getString("name")
+// val age = person.getInt("age")
+           
+           
+           //another option based off
+//                // Find a dog to update.
+//     val dog = bgRealm.where<Dog>().equalTo("age", 1.toInt()).findFirst()!!
+//     dog.age = 3 // Update its age value.
+           
+           val realmItem =  RealmWrapper.realm.where(MapNames::class.java)
+            .equalTo("theMarkerLocation.row",x)
+            .equalTo("theMarkerLocation.col", y +1 )
+            .findFirst()!!
+           return realmItem.distanceNearMarker
+           
+           
         var distanceToNearestMarker = 0.0
         return RealmWrapper.realm.where(MapNames::class.java)
             .equalTo("theMarkerLocation.row",x)
